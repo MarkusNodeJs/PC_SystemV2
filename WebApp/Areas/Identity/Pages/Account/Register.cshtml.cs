54f98c63f162a02d80ac0c449c7d9bdf568f2372
MarkusNodeJs <markus.dalton03282023@gmail.com>
2023-06-16 14:28:41 +0800

    update

--- WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -130,14 +130,14 @@ namespace WebApp.Areas.Identity.Pages.Account
             {
                 var user = new AccountUser
                 {
-                    UserName = Input.Email,
+                    UserName = Input.LastName,
                     Email = Input.Email,
                     FirstName = Input.FirstName,
                     MiddleName = Input.MiddleName,
                     LastName = Input.LastName
                 };
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.LastName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
